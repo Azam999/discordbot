c1f508f42c9868493f3094a3fd3fb0b48f080035
Azam Ahmed <aziskool20@gmail.com>
2021-09-17 20:09:43 -0400

    Bot now returns a random quote from the api

--- main.py
+++ main.py
@@ -2,7 +2,39 @@ import discord
 import os
 import base64
 from dotenv import load_dotenv
+import requests
 
+# ===== HELPER FUNCTIONS =====
+def get_quote():
+  quote_data = requests.get("https://api.quotable.io/random").json()
+  
+  def get_tags():
+    tags_list = quote_data['tags']
+    
+    if (len(tags_list) <= 1):
+      return tags_list[0]
+    
+    tags = ""
+    for tag in tags_list:
+      tags += f"{tag}, "
+    tags = tags.rstrip()[:-1]
+    
+    return tags
+  
+  tags = get_tags()
+  quote_content = quote_data['content']
+  quote_author = quote_data['author']
+  
+  full_quote = f'''
+  {quote_content} ({tags})
+  - {quote_author}
+  '''
+  
+  return full_quote
+
+
+
+# Create discord client
 client = discord.Client()
 
 # Runs when bot initializes
@@ -19,8 +51,9 @@ async def on_message(message):
   if message.author == client.user:
     return
   
+  quote = get_quote()
   if message.content.startswith('$quote'):
-    await message.channel.send("joke")
+    await message.channel.send(quote)
 
 # Load env vars
 load_dotenv()
